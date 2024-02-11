4505aefde5a431565cac0c9fe66f45fadecd94a5
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-10 21:16:28 -0500

    Improved Console.py

--- src/section1/Console.py
+++ src/section1/Console.py
@@ -1,5 +1,8 @@
 def main() -> None:
-    """Print student data in a formatted table."""
+    """Prompt user for name and age, then print the input.
+
+    :return: None
+    """
     try:
         name = input("Enter name: ")
         age = int(input("Enter age: "))
