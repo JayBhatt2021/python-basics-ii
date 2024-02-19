9324462034a74c9436564b2a8d2f83d69abfae34
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-19 17:17:30 -0500

    Corrected SumLists.py

--- src/section5/SumLists.py
+++ src/section5/SumLists.py
@@ -2,13 +2,12 @@ from typing import List
 
 
 class Node:
-    """A class representing a node."""
+    """A class representing a node in a doubly linked list."""
 
     def __init__(self, num: int) -> None:
-        """Initialize a bird.
+        """Initialize a node with a value.
 
-        :param num: The name of the bird species.
-        :return: None
+        :param num: The value of the node.
         """
         self.num = num
         self.previous = None
@@ -16,19 +15,20 @@ class Node:
 
 
 class DoublyLinkedList:
-    """A class representing a bird survey."""
+    """A class representing a doubly linked list."""
 
     def __init__(self, head: Node = None) -> None:
-        """Initialize the bird survey.
+        """Initialize an instance of DoublyLinkedList with an optional head
+        node.
 
-        :return: None
+        :param head: The head node of the list.
         """
         self.head = head
 
     def __str__(self) -> str:
-        """Initialize the bird survey.
+        """Return a string representation of DoublyLinkedList.
 
-        :return: Replace this
+        :return: The string representation of DoublyLinkedList.
         """
         current, str_representation = self.head, ""
 
@@ -40,31 +40,30 @@ class DoublyLinkedList:
         return str_representation
 
     def append(self, num: int) -> None:
-        """Add a bird to the survey or increment its count if already present.
+        """Add a node with the given value to the end of the list.
 
-        :param species_name: The name of the bird species to add.
-        :return: None
+        :param num: The value of the node to append.
         """
         new_node = Node(num)
 
-        # Initializes the head if the linked list is empty
+        # Initializes the head if the doubly linked list is empty
         if not self.head:
             self.head = new_node
             return
 
-        # Initializes the head if the linked list is empty
-        current = self.head
-        while current.next:
+        previous, current = None, self.head
+        while current:
+            previous = current
             current = current.next
 
-        # Initializes the head if the linked list is empty
-        current.next = new_node
-        new_node.previous = current
+        # Creates a double-link between the previous and new nodes
+        previous.next = new_node
+        new_node.previous = previous
 
     def calculate_sum(self) -> int:
-        """Print the bird survey.
+        """Calculate the sum of all values in the doubly linked list.
 
-        :return: None
+        :return: The sum of all values.
         """
         current, total = self.head, 0
 
@@ -76,64 +75,45 @@ class DoublyLinkedList:
 
 
 def calculate_list_sum(num_list: List[int]) -> int:
-    """Print the bird survey.
+    """Calculate the sum of all numbers in a list.
 
-    :return: None
+    :param num_list: The list of numbers.
+    :return: The sum of all numbers.
     """
     return sum(num_list)
 
 
 def main() -> None:
-    """Generate two lists of numbers, search for a user-provided key, and print
-    the indices where the key is found.
-
-    :return: None
-    """
+    """Main function to run the program."""
     try:
-        # --- List ---
-        while True:
-            list_count = int(
-                input(
-                    "How many integers do you want to add to the list (max is "
-                    "10)?: "
-                )
+        # Input and Sum Calculation for Regular List
+        list_size = int(
+            input(
+                "How many integers do you want to add to the list (max is 10)?"
+                ": "
             )
-
-            if list_count <= 10:
-                break
-            print("\nYou cannot enter more than 10 integers.")
-
-        num_list = []
-        print("\nList Numbers:")
-        for i in range(1, list_count + 1):
-            num = int(input(f"Enter integer #{i}: "))
-            num_list.append(num)
-
+        )
+        list_size = min(list_size, 10)
+        num_list = [
+            int(input(f"Enter integer #{i}: ")) for i in range(1, list_size + 1)
+        ]
         print(f"\nList: {num_list}")
         print(
             f"The sum of the numbers in the list is "
             f"{calculate_list_sum(num_list)}.\n"
         )
 
-        # --- Doubly Linked List ---
-        while True:
-            linked_list_count = int(
-                input(
-                    "How many integers do you want to add to the doubly "
-                    "linked list (max is 10)?: "
-                )
+        # Input and Sum Calculation for Doubly Linked List
+        linked_list_size = int(
+            input(
+                "How many integers do you want to add to the doubly linked "
+                "list (max is 10)?: "
             )
-
-            if linked_list_count <= 10:
-                break
-            print("\nYou cannot enter more than 10 integers.")
-
+        )
+        linked_list_size = min(linked_list_size, 10)
         num_linked_list = DoublyLinkedList()
-        print("\nDoubly Linked List Numbers:")
-        for i in range(1, linked_list_count + 1):
-            num = int(input(f"Enter integer #{i}: "))
-            num_linked_list.append(num)
-
+        for i in range(1, linked_list_size + 1):
+            num_linked_list.append(int(input(f"Enter integer #{i}: ")))
         print(f"\nDoubly Linked List: {num_linked_list}")
         print(
             f"The sum of the numbers in the doubly linked list is "
